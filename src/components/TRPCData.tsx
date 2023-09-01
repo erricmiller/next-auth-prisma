7b7b35cfbad1ff96ab9ae77629925900c7b3503d
erricmiller <131812092+erricmiller@users.noreply.github.com>
2023-08-31 17:31:32 -0700

    Added next-auth

--- src/components/TRPCData.tsx
+++ src/components/TRPCData.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { trpc } from '@/app/_trpc/client';
+import { prisma } from '@/lib/prisma';
 import React from 'react'
 
 const TRPCData =  () => {
     const todos = trpc.getTodos.useQuery();
 
+
   return (
-    <div>"JSON.stringify(todos.data)"</div>
+    <div>{JSON.stringify(todos.data)}</div>
   )
 }
 
--- src/server/index.ts
+++ src/server/index.ts
@@ -3,20 +3,23 @@ import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
-    const data = await prisma.data.create({
-      data:{
-        // id: 1,
-      name: "Fahad",
-      email: "fahad@test.com",
-      age: 24,
-      gender: "Male",
-      price: 99,
-      card_type: "jcb",
-      date: "20/5/2022",
-    }
-    });
+    console.log("Storing data")
+    // const data = await prisma.data.create({
+    //   data:{
+    //     // id: 1,
+    //   name: "Fahad",
+    //   email: "fahad@test.com",
+    //   age: 24,
+    //   gender: "Male",
+    //   price: 99,
+    //   card_type: "jcb",
+    //   date: "20/5/2022",
+    // }
+    // });
+
+    const data = await prisma.data.findMany()
     console.log(data)
-    return [10,20,30];
+    return data;
   }),
 });
 
